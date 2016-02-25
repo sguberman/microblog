7635af525c25892ae77287fb0fda4180be93d14b
Seth Guberman <sguberman@gmail.com>
2016-02-25 18:24:40 +0000

    implement a simple template for the home page

--- app/views.py
+++ app/views.py
@@ -1,6 +1,8 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'nickname': 'Seth'}  # fake user
+    return render_template('index.html', title='Home', user=user)
