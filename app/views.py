2076bdbc695ab3a8cafbbcfd4e0421572fc2cbd1
Seth Guberman <sguberman@gmail.com>
2016-02-25 18:43:54 +0000

    add navigation bar and make index.html inherit from base.html

--- app/views.py
+++ app/views.py
@@ -1,8 +1,22 @@
 from flask import render_template
 from app import app
 
+
 @app.route('/')
 @app.route('/index')
 def index():
     user = {'nickname': 'Seth'}  # fake user
-    return render_template('index.html', title='Home', user=user)
+    posts = [  # fake array of posts
+             {
+                 'author': {'nickname': 'John'},
+                 'body': 'Beautiful day in Jersey!'
+             },
+             {
+                 'author': {'nickname': 'Susan'},
+                 'body': 'The Avengers movie was pretty good.'
+             }
+            ]
+    return render_template('index.html',
+                           title='Home',
+                           user=user,
+                           posts=posts)
