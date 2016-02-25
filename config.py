58c37cf0e61b490671bcd6354cf301a4d7f8a9d0
Seth Guberman <sguberman@gmail.com>
2016-02-25 21:25:04 +0000

    create basic db management scripts

--- config.py
+++ config.py
@@ -1,3 +1,6 @@
+import os
+
+
 WTF_CSRF_ENABLED = True
 SECRET_KEY = 'you-will-never-guess'
 
@@ -6,3 +9,8 @@ OPENID_PROVIDERS = [
     {'name': 'Yahoo', 'url': 'https://me.yahoo.com'},
     {'name': 'MyOpenID', 'url': 'https://www.myopenid.com'},
 ]
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
