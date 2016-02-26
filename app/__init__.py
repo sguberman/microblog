d2087b04d2153502b5b22f905cd801b28a147ba1
Seth Guberman <sguberman@gmail.com>
2016-02-26 19:41:37 +0000

    add file logging

--- app/__init__.py
+++ app/__init__.py
@@ -13,4 +13,21 @@ lm = LoginManager()
 lm.init_app(app)
 lm.login_view = 'login'
 oid = OpenID(app, os.path.join(basedir, 'tmp'))
+
+# Logging to a file
+if not app.debug:
+    import logging
+    from logging.handlers import RotatingFileHandler
+    file_handler = RotatingFileHandler('tmp/microblog.log',
+                                       'a',
+                                       1 * 1024 * 1024,
+                                       10)
+    fmtstr = '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'
+    fmt = logging.Formatter(fmtstr)
+    file_handler.setFormatter(fmt)
+    app.logger.setLevel(logging.INFO)
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+    app.logger.info('microblog startup')
+
 from app import views, models
